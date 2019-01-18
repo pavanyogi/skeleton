0b7553a0616485d6394b4e69fe8e8a3b80c7feaa
Pavan Yogi <pavany@mindfiresolutions.com>
2019-01-18 18:24:00 +0530

    (dev-1)-added working sitepoint phpunit testing example

---
+++ tests/SitePoint/Converter/ConverterTest.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace SitePoint\Converter;
+
+use PHPUnit\Framework\TestCase;
+
+class ConverterTest extends TestCase {
+
+    public function testHello() {
+        $this->assertEquals('Hello', 'Hell' . 'o');
+    }
+
+    public function testSimpleConversion()
+    {
+        $input = '{"key":"value","key2":"value2"}';
+        $output = [
+            'key' => 'value',
+            'key2' => 'value2'
+        ];
+        
+        // $input     = '{"key":"value","key2":"value2","some-array":[1,2,3,4,5]}';
+        // $output    = [
+        //     'key'        => 'value',
+        //     'key2'       => 'value2',
+        //     'some-array' => [1, 2, 3, 4, 5],
+        // ];
+
+        $converter = new Converter();
+        $this->assertEquals($output, $converter->convertString($input));
+    }
+
+    // public function testMoreComplexConversion()
+    // {
+    //     $input     = '{"key":"value","key2":"value2","some-array":[1,2,3,4,5],"new-object":{"key":"value","key2":"value2"}}';
+    //     $output    = [
+    //         'key'        => 'value',
+    //         'key2'       => 'value2',
+    //         'some-array' => [1, 2, 3, 4, 5],
+    //         'new-object' => [
+    //             'key'  => 'value',
+    //             'key2' => 'value2',
+    //         ],
+    //     ];
+    //     $converter = new Converter();
+    //     $this->assertEquals($output, $converter->convertString($input));
+    // }
+
+    // public function testMostComplexConversion()
+    // {
+    //     $input     = '[{"key":"value","key2":"value2","some-array":[1,2,3,4,5],"new-object":{"key":"value","key2":"value2"}},{"key":"value","key2":"value2","some-array":[1,2,3,4,5],"new-object":{"key":"value","key2":"value2"}},{"key":"value","key2":"value2","some-array":[1,2,3,4,5],"new-object":{"key":"value","key2":"value2"}}]';
+    //     $output    = [
+    //         [
+    //             'key'        => 'value',
+    //             'key2'       => 'value2',
+    //             'some-array' => [1, 2, 3, 4, 5],
+    //             'new-object' => [
+    //                 'key'  => 'value',
+    //                 'key2' => 'value2',
+    //             ],
+    //         ],
+    //         [
+    //             'key'        => 'value',
+    //             'key2'       => 'value2',
+    //             'some-array' => [1, 2, 3, 4, 5],
+    //             'new-object' => [
+    //                 'key'  => 'value',
+    //                 'key2' => 'value2',
+    //             ],
+    //         ],
+    //         [
+    //             'key'        => 'value',
+    //             'key2'       => 'value2',
+    //             'some-array' => [1, 2, 3, 4, 5],
+    //             'new-object' => [
+    //                 'key'  => 'value',
+    //                 'key2' => 'value2',
+    //             ],
+    //         ],
+    //     ];
+    //     $converter = new Converter();
+    //     $this->assertEquals($output, $converter->convertString($input));
+    // }
+
+}
